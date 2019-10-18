bffe971d2fabc2f0a4d817fccd9b0b7755a61cdd
ge29lip@ForStudents.onmicrosoft.com <ge29lip@ForStudents.onmicrosoft.com>
2019-10-18 11:37:03 +0200

    intelj setting

--- Preproceesing.py
+++ Preproceesing.py
@@ -1,13 +1,18 @@
 import os, json
+import csv
 
 # path_to_json = '/Users/weiding/Google Drive/Application Project/100_files'
-path_to_json = '/Users/weiding/Desktop/vc/sum_all'
+path_to_json = 'C:/Users/kwokp/OneDrive/Desktop/Study/zzz_application project/vc/test'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
 count = 0
 for index, js in enumerate(json_files):
     with open(os.path.join(path_to_json, js)) as json_file:
         json_text = json.load(json_file)
+        #print(js)
+        #print(json_text.keys())
+        for key in json_text.keys():
+            print(key)
         if len(json_text) == 0:
             count += 1
 
