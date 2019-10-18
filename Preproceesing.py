fb4ad30ee9d084b215df361431958c001ec5c231
wei <wei.ding@tum.de>
2019-10-18 11:04:41 +0200

    clean code

--- Preproceesing.py
+++ Preproceesing.py
@@ -2,13 +2,15 @@ import os, json
 
 # path_to_json = '/Users/weiding/Google Drive/Application Project/100_files'
 path_to_json = '/Users/weiding/Desktop/vc/sum_all'
+# read all json files from the folder
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
-count = 0
+# enumerate each file and find number of empty files
+count_empty_file = 0
 for index, js in enumerate(json_files):
     with open(os.path.join(path_to_json, js)) as json_file:
         json_text = json.load(json_file)
         if len(json_text) == 0:
-            count += 1
+            count_empty_file += 1
 
-print(count)
+print(count_empty_file)
